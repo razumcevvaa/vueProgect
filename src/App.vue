ee826bb4790083831ee1fb17f64e74cd92d7f68d
razumcevvaa <katerazumceva@gmail.com>
2024-08-08 21:13:07 +0300

    vue new

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 :style="'color:#'+color" @click="str += '!', colorFun">{{ str }}</h1>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+const str = ref('Learn vue')
+const color = ref(77777)
+color.value = 77777
+const colorFun = () => {
+  color.value += 150000
+
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1 {
+  user-select: none;
 }
 </style>
