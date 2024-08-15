34b4bcc19e29a0ad8156502bafe25477917d58c8
razumcevvaa <katerazumceva@gmail.com>
2024-08-15 20:49:12 +0300

    calk

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <h1 :style="'color:#'+color" @click="str += '!', colorFun">{{ str }}</h1>
+  <VueCalc></VueCalc>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import VueCalc from './components/DelegatedCalc.vue'
 const str = ref('Learn vue')
 const color = ref(77777)
 color.value = 77777
 const colorFun = () => {
   color.value += 150000
-
 }
+
 </script>
 
 <style scoped>
