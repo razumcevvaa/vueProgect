7438446c7fcf42a694bfb4718a764bed31f23119
razumcevvaa <katerazumceva@gmail.com>
2024-08-22 18:39:58 +0300

    todolist

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 :style="'color:#' + color" @click="str += '!', colorFun">{{ str }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const str = ref('Learn vue')
+const color = ref(77777)
+color.value = 77777
+
+const colorFun = () => {
+  color.value += 150000
+}
+</script>
+
+<style>
+h1 {
+  user-select: none;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="add">
+      <input v-model="message" type="text" placeholder="ToDo">
+    </form>
+    <select id="todo" v-model="select">
+      <option value="All">Все</option>
+      <option value="Completed">done</option>
+      <option value="Cancelled">Отмененные</option>
+      <option value="Deleted">Удаленные</option>
+    </select>
+    <ul>
+      <li :data-check="el.check" v-for="el of arr" :key="el.id">{{ el.text }}
+        <span v-if="el.check == 0">
+          <button @click="check(el.id, 1)">done</button>
+          <button @click="check(el.id, 2)">cancel</button>
+          <button @click="check(el.id, 3)">remove</button>
+        </span>
+      </li>
+    </ul>
+    <button @click="clean">clear</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const select = ref('All')
+const message = ref('')
+const localArr = localStorage.arr ? JSON.parse(localStorage.arr) : []
+const arr = ref(localArr as any[])
+
+const add = () => {
+  arr.value.push(
+    { text: message.value, id: arr.value.length, check: 0 }
+  )
+  localStorage.arr = JSON.stringify(arr.value)
+  message.value = ''
+}
+
+const clean = () => {
+  localStorage.clear()
+  //и сделать автомат перезагрузку
+}
+
+const check = (id: number, check: number) => {
+  const el = arr.value.find(el => el.id == id)
+  el.check = check
+  localStorage.arr = JSON.stringify(arr.value)
+}
+
+const choice = computed(() => {
+  if (select.value == 'All') {
+    return select.value
+  } else {
+    arr.value.filter((el) => el.text.startsWith(select.value))
+  }
+})
+console.log(choice)
+</script>
+
+
+<style>
+[data-check="1"] {
+  text-decoration: line-through;
+}
+
+[data-check="2"] {
+  background-color: red;
+}
+
+[data-check="3"] {
+  background-color: green;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 50px 0;
+}
+
+input {
+  width: 350px;
+  height: 20px;
+}
+
+ul {
+  width: 300px;
+}
+select{
+  width: 100px;
+}
+</style>
