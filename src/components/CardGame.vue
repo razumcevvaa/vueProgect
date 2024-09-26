518e0b690b9b17fbc01c68829b2ae22ba3ca3da2
razumcevvaa <katerazumceva@gmail.com>
2024-09-27 00:18:40 +0300

    css game card

--- src/components/CardGame.vue
+++ src/components/CardGame.vue
@@ -2,8 +2,8 @@
   <div class="wrap">
     <div class="game">
       <div v-for="card in cards" :key="card.id" @click="flip(card)" class="card">
-        <img :src="card.img" />
-        <img src="/imgBack.jpeg" />
+        <img :src="card.img" class="back-face" />
+        <img src="/imgBack.jpeg" class="front-face" />
       </div>
     </div>
   </div>
@@ -13,24 +13,24 @@
 import { ref } from 'vue'
 let flipped = ''
 
-const flip = (card) =>{
+const flip = (card:any) => {
   if (!flipped) {
     flipped = card.img
     card.isOpened = true
   } else {
     card.isOpened = true
-    if (flipped==card.img) {
+    if (flipped == card.img) {
       flipped = ''
       // vse verno nichego ne delaem
       console.log('ok')
     } else {
       console.log('not ok')
       flipped = ''
-      setTimeout(()=>{
+      setTimeout(() => {
         for (let card of cards.value) {
-          if (card.isOpened) card.isOpened=!card.isOpened
+          if (card.isOpened) card.isOpened = !card.isOpened
         }
-      },1000)
+      }, 1000)
     }
   }
 }
@@ -64,61 +64,74 @@ const cards = ref([
   {
     img: "./card4.jpeg",
     isOpened: false,
-    id: 4,
+    id: 6,
   },
   {
     img: "./card5.jpeg",
     isOpened: false,
-    id: 5,
+    id: 7,
   },
   {
     img: "./card2.jpeg",
     isOpened: false,
-    id: 2,
+    id: 8,
   },
   {
     img: "./card1.jpeg",
     isOpened: false,
-    id: 1,
+    id: 9,
   },
   {
     img: "./card3.jpeg",
     isOpened: false,
-    id: 3,
+    id: 10,
   },
 ])
 
 </script>
 <style scoped>
-
 .game {
-  display: flex;
-  flex-wrap: wrap;
+  /* display: flex;
+  flex-wrap: wrap; */
+  width: 1200px;
+    display: grid;
+    grid-template-columns: 250px 250px 250px 250px 250px;
+    grid-template-rows: 250px 250px;
+    gap: 20px;
 }
 
 .wrap {
-  position: relative;
   padding: 0;
   width: 1400px;
 }
 
 img {
   width: 250px;
+  height: 250px;
 }
 
 .card {
-  padding: 10px;
+  position: relative;
 }
-.card .inside {
-  width: 100%;
-  height: 100%;
-  display: block;
-  transform-style: preserve-3d;
-  transition: 0.4s ease-in-out;
-  background: white;
-}
-.card .front{
-  display: none;
+.front-face {
+        transform: rotate3d(0, 1, 0, 0deg);
+        transition: 1s 1s;
+    }
+.back-face {
+        transform: rotate3d(0, 1, 0, 90deg);
+        transition: 1s ;
+    }
+.front-face {
+  /* position: absolute; */
+  border-radius: 5px;
+  transform: rotate3d(0, 1, 0, 0deg);
+  transition: 1s 1s;
 }
 
+.back-face {
+  position: absolute;
+  border-radius: 5px;
+  transform: rotate3d(0, 1, 0, 90deg);
+  transition: 1s;
+}
 </style>
